7581a17fb170bfc885edc5b4817fb13cbc9cc570
ankit-kumar <143241108+student-appnacollege@users.noreply.github.com>
2025-07-24 10:59:35 +0530

    Add files via upload

---
+++ function.py
@@ -0,0 +1,10 @@
+def avg():
+   a = int(input("enter the number:"))
+   b= int(input("enter the number:"))
+   c= int(input("enter the number:"))
+
+   av=(a+b+c)/3
+   print(av)
+avg()
+print("hello mumbai")
+avg()
